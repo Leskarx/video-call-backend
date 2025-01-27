03cab2e024ce3f66d25fd3f07d1abdf96bc3df3d
LeskarX <leskarzone123@gmail.com>
2025-01-28 01:49:02 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 app.use(cors());
 const io = new Server(server,{
     cors:{
-        origin:"*",
+        origin:"http://localhost:5173/",
         methods:["GET","POST"]
     }
 });
